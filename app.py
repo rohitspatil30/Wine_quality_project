c7fdb7319d60a8881942c6b55aee4254a4d83420
rohitspatil30 <rohitspatil30@gmail.com>
2024-04-28 20:07:32 +0530

    model evaluation added

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-'''
 from flask import Flask, render_template, request
 import os 
 import numpy as np
 import pandas as pd
-from mlProject.pipeline.prediction import PredictionPipeline
+from src.wine_quality_prediction.pipeline.prediction import PredictionPipeline
 
 
 app = Flask(__name__) # initializing a flask app
@@ -60,4 +59,3 @@ def index():
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port = 8080)
-'''
